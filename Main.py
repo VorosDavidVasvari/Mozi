7aac7df9f64a94dafde96a5e7d87de30411cdb31
VorosDavid <c2vordav@vasvai.org>
2025-01-27 12:17:49 +0100

    added random seat fillup

--- Main.py
+++ Main.py
@@ -1,12 +1,25 @@
+import random
+
+POSSIBLE_SEAT_STATES = ['A', 'S', 'K', 'E']
+
 def InitSeats(seats: list) -> list:
     for i in range(15):
         seats.append([[], []])
 
     return seats
 
+def FillUpSeats(seats: list) -> list:
+    for i in range(len(seats)):
+        for j in range(len(seats[i])):
+            for k in range(10):
+                seats[i][j].append(POSSIBLE_SEAT_STATES[random.randint(0, 3)])
+
+    return seats
+
 def Main():
     seats: list = []
     InitSeats(seats)
+    FillUpSeats(seats)
 
 if __name__ == "__main__":
     Main()
