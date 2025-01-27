b477fddf9c68d7d1578d562218e073fccf165a3b
VorosDavid <c2vordav@vasvai.org>
2025-01-27 12:13:32 +0100

    init seat func

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
+def InitSeats(seats: list) -> list:
+    for i in range(15):
+        seats.append([[], []])
+
+    return seats
+
 def Main():
-    print("Hello, World!")
+    seats: list = []
+    InitSeats(seats)
 
 if __name__ == "__main__":
     Main()
